d14a4924a59ba7e7c3f47da9b0f8183051a31f84
Grace Gately <gracemgately@gmail.com>
2018-05-26 16:26:41 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'cats',
+        description: 'There are so many cats, how can I possibly describe them all?',
+        image: './assets/cat1.jpg',
+        isPurring: true, 
+        details: ['mischievous', 'affectionate', 'clever', 'sharp-toothed']
+    }
+})
